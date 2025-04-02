3407499872b135fcc15963f40b4a1370674aa7cf
TotixT <santiagolopezgarcia22@gmail.com>
2025-04-01 19:46:32 -0500

    UwU

--- src/components/EditarTareaModal.jsx
+++ src/components/EditarTareaModal.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "../css/EditarTareaModal.css";
 
 function EditarTareaModal({ tarea, onClose, onUpdate, empleados }) {
